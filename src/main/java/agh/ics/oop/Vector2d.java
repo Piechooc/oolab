b2729e7d66b3f5f78b267258d5b769a920c38a18
Piechooc <piechowkamarta@gmail.com>
2021-10-23 22:18:09 +0200

    first commit

---
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,66 @@
+package agh.ics.oop;
+
+import java.util.Objects;
+
+public class Vector2d {
+    final public int x;
+    final public int y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + " " + y + ")";
+    }
+
+    public boolean precedes(Vector2d other) {
+        return this.x >= other.x && this.y >= other.y;
+    }
+
+    public boolean follows(Vector2d other) {
+        return this.x <= other.x && this.y <= other.y;
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        if (this.x >= other.x && this.y >= other.y) {
+            return this;
+        } else {
+            return other;
+        }
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        if (this.x >= other.x && this.y >= other.y) {
+            return other;
+        } else {
+            return this;
+        }
+    }
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector2d vector2d = (Vector2d) o;
+        return x == vector2d.x && y == vector2d.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-1 * this.x, -1 * this.y);
+    }
+}
---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,69 @@
+package agh.ics.oop;
+
+import java.util.ArrayList;
+
+import static agh.ics.oop.Direction.*;
+
+public class World {
+    public static void main(String[] args) {
+//        System.out.println("Start");
+//        ArrayList<Direction> newArgs = convert(args);
+//        run(newArgs);
+//        System.out.println("Stop");
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+    }
+
+    public static void run(ArrayList<Direction> arg) {
+//        System.out.println("Animal goes forward");
+//        for (int i = 0; i < arg.length; i++) {
+//            if (i != arg.length - 1) {
+//                System.out.print(arg[i]);
+//                System.out.print(", ");
+//            } else {
+//                System.out.println(arg[i]);
+//            }
+//        }
+
+//        for(String el : arg) {
+//            String res = switch(el) {
+//                case "f" -> "Zwierzak idzie do przodu";
+//                case "b" -> "Zwierzak idzie do tyłu";
+//                case "r" -> "Zwierzak skręca w prawo";
+//                case "l" -> "Zwierzak skręca w lewo";
+//                default -> "-1";
+//            };
+//
+//            System.out.println(res);
+//        }
+
+        for (Direction el : arg) {
+            String res = switch (el) {
+                case FORWARD -> "Zwierzak idzie do przodu";
+                case BACKWARDS -> "Zwierzak idzie do tyłu";
+                case RIGHT -> "Zwierzak skręca w prawo";
+                case LEFT -> "Zwierzak skręca w lewo";
+            };
+
+            System.out.println(res);
+        }
+    }
+
+    public static ArrayList<Direction> convert(String[] arg) {
+        ArrayList<Direction> l = new ArrayList<>();
+
+        for (String el : arg) {
+            switch (el) {
+                case "f" -> l.add(FORWARD);
+                case "b" -> l.add(BACKWARDS);
+                case "r" -> l.add(RIGHT);
+                case "l" -> l.add(LEFT);
+            }
+        }
+
+        return l;
+    }
+}
